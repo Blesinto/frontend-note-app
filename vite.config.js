0be41823e1260fe78f8f2560c9130cd8e09559ce
Anakor Joshua <becomingadev@gmail.com>
2024-09-04 12:55:51 +0100

    Your third  commit

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      plugins: [visualizer()],
+      external: ['react-icons/md'],
     },
   },
 });
